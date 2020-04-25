e39b1c3ac3bb03ceda0a1b80d517e2f27f716e2e
Gerard K <n/a>
2020-04-25 21:50:29 +0200

    Extract buffer out of SerialCommands

--- src/serial_commands.rs
+++ src/serial_commands.rs
@@ -1,5 +1,59 @@
 use core::str;
 
+const BUFFER_SIZE: usize = 20;
+type BufferType = [u8; BUFFER_SIZE]; 
+struct Buffer {
+    buffer: BufferType,
+    next_position: usize,
+    last_position: usize,
+}
+
+impl Default for Buffer {
+    fn default() -> Self {
+        Self {
+            buffer: [0; BUFFER_SIZE],
+            next_position: 0,
+            last_position: 0,
+        }
+    }
+}
+
+impl Buffer
+{
+    pub fn add_character(&mut self, data: u8) {
+        self.buffer[self.next_position] = data;
+        
+        self.last_position = self.next_position;
+        self.next_position += 1;
+
+        if self.next_position >= BUFFER_SIZE {
+            self.next_position = 0;  
+        }
+    }
+
+    pub fn last_char(&self) -> u8 {
+        self.buffer[self.last_position]
+    }
+
+    pub fn create_arranged_buffer(&self) -> BufferType {
+        let mut parse_buffer: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
+
+        let left = &self.buffer[..self.last_position];
+        let right = &self.buffer[self.last_position..];
+
+        // Arrange into the parse buffer.
+        for (global_buffer, parse_buffer) in right.iter().chain(left.iter())
+            .zip(parse_buffer.iter_mut()) {
+                *parse_buffer = *global_buffer;
+        }
+        parse_buffer
+    }
+
+    pub fn reset(&mut self) {
+        self.buffer.iter_mut().for_each(|x| *x = 0);
+    }
+}
+
 #[derive(Debug)]
 #[derive(PartialEq)]
 #[derive(Clone)]
@@ -8,24 +62,17 @@ pub enum Command {
     Right(u32),
 }
 
-const BUFFER_SIZE: usize = 20;
-type BufferType = [u8; BUFFER_SIZE];
 pub struct SerialCommands
-    //where CB: FnMut(Command) -> ()
 {
-    buffer: BufferType,
-    position: usize,
-    command: Option<Command>,
-    //callback: CB,
+    buffer: Buffer,
+    //command: Option<Command>,
 }
 
 impl Default for SerialCommands {
     fn default() -> Self {
         Self {
-            buffer: [0; BUFFER_SIZE],
-            position: 0,
-            command: None,
-            //callback,
+            buffer: Buffer::default(),
+            //command: None,
         }
     }
 }
@@ -52,56 +99,24 @@ impl SerialCommands
     //where CB: FnMut(Command) -> ()
 {
     pub fn add_character(&mut self, data: u8) {
-        self.store_character(data);
+        self.buffer.add_character(data);
     }
 
-    pub fn get_command(&mut self) -> Option<Command>
-    {
-        let command = self.command.as_ref()?;
-        let command = command.clone();
-        self.command = None;
+    pub fn get_command(&mut self) -> Option<Command> {
+        if self.buffer.last_char() == CARRIAGE_RETURN_VALUE {
+            let parse_buffer = self.buffer.create_arranged_buffer();
 
-        Some(command)
-    }
-
-    fn store_character(&mut self, data: u8) {
-        self.buffer[self.position] = data;
-        self.check_for_command();
-
-        // Increment buffer
-        self.position += 1;
-
-        if self.position >= BUFFER_SIZE {
-            self.position = 0;  
-        }
-    }
-
-    fn check_for_command(&mut self) {
-        if self.buffer[self.position] == CARRIAGE_RETURN_VALUE {
-            let mut parse_buffer: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
-
-            let left = &self.buffer[..self.position];
-            let right = &self.buffer[self.position..];
-
-            // Arrange into the parse buffer.
-            for (global_buffer, parse_buffer) in right.iter().chain(left.iter())
-                .zip(parse_buffer.iter_mut()) {
-                    *parse_buffer = *global_buffer;
+            let command = self.parse_commands(&parse_buffer);
+            if command.is_some()
+            {
+                self.buffer.reset();
+                return command;
             }
-
-            // Parse
-            self.parse_commands(&parse_buffer);
-
-            // Clear the buffer
-            self.buffer.iter_mut().for_each(|x| *x = 0);
         }
+        None
     }
-
-    pub fn get_buffer(&self) ->&[u8] {
-        &self.buffer
-    }
-
-    fn parse_commands(&mut self, buffer: &[u8]) {
+    
+    fn parse_commands(&self, buffer: &[u8]) -> Option<Command>{
         if let Ok(parse_buffer) = str::from_utf8(buffer) {
             let command_parts = parse_buffer.split_terminator(
                 |c:char| !c.is_ascii_digit() && !c.is_ascii_alphanumeric());
@@ -110,12 +125,12 @@ impl SerialCommands
                 {
                     if available_commands.label == command
                     {
-                        self.command = Some(Command::Left(0));
+                        return Some(Command::Left(0));
                     }
                 }
             }
         }
-
+        None
     }
 }
 
